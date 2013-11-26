b00e0379d7bff6b1da62e5f26c477b5861f32cd5
Johnson Chang <johnson2012@gmail.com>
2013-11-26 09:37:05 -0500

    fixed bugs of data preprocessing class

--- src/Summary.java
+++ src/Summary.java
@@ -1,6 +1,13 @@
+import java.io.UnsupportedEncodingException;
 import java.net.URL;
+import java.sql.Date;
+import java.sql.SQLException;
+import java.text.ParseException;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
+
+import Date.DateTime;
 
 import com.google.code.jconfig.ConfigurationManager;
 import com.google.code.jconfig.listener.IConfigurationChangeListener;
@@ -18,15 +25,31 @@ public class Summary {
 		listeners.put("server_list", new ServerConfigurationContainer());
 		ConfigurationManager.configureAndWatch(listeners, configPath, 200L);
     }
-    
-	public static void main(String[] args) {
-		String dbURL    = "jdbc:mysql://localhost:3306/nlp";
-		String username = "root";
-		String password = "";
-		MySQL db = new MySQL();
-		boolean test = db.connect(dbURL, username, password);
-		System.out.println(test);
-		db.close();
+        
+	public static void main(String[] args) throws ParseException, UnsupportedEncodingException {
+		DateTime test = new DateTime();
+		//String value = "Sunday, September 29, 2013 7:59:58 AM PDT";
+		//String value = "Sunday, September 29, 2013 7:59 AM";
+		//String value = "Tue, Jul 9, 2013 3:20 PM";
+		//String value = "Tue, Jul 9, 2013 3:20 PM";
+		String value = "Tue, Jul 9, 2013 3:20 PM";
+		//String value = "Tue, Jul 9, 2013 3:20 PM";
+		//test.dateTimeFormat(value);
+		String newString = "";
+		for (String s : value.split("")) {
+			//System.out.println(c + " <> " + Character.getNumericValue(c));
+			System.out.println(s);
+			//value += s;
+			//System.out.println(' ' + " <> " + Character.getNumericValue(' '));
+			//if (c != ',' && Character.getNumericValue(c) == -1) newString += ' ';
+			//else newString += c;
+			//newString += Character.;
+		}
+		//newString = new String(value.getBytes("UTF-8"), "Windows-1252");
+		//newString = new String(newString.getBytes("Windows-1252"), "Windows-1252");
+		
+		newString = value.replaceAll("([\\w]+),\\S([\\w]+)\\S([\\d]{1,2}),\\S([\\d]{1,4})\\S([\\d]{1,2}:[\\d]{1,2})\\S([aApP][mM])", "$1, $2 $3, $4 $5 $6");
+		System.out.println(test.getDateInFormat(newString, "EEE, MMM dd, yyyy hh:mm aa"));
 	}
 
 }
--- src/parser/RegexMatches.java
+++ src/parser/RegexMatches.java
@@ -1,48 +1,34 @@
 package parser;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.regex.Pattern;
 import java.util.regex.Matcher;
 
 public class RegexMatches {
-	private static final String EmailStart  = "(([\\w]+[\\S\\s])+<.+>)(.+[\\S\\s][aAmM])";
-	private static final String Receivers   = "To: (.+)";
+	//private static final String EmailStart  = "(([\\w]+[\\S\\s])+<.+@.+>)(.+[\\S\\s][aAmM])";
+	private static final String EmailStart  = "(.+<[_A-Za-z0-9\\-\\+]+(\\.[_A-Za-z0-9\\-]+)*@[A-Za-z0-9\\-]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})>)(\\w{3},\\S\\w{3}\\S\\d{1,2},\\S\\d{1,4}\\Sat\\S\\d{1,2}:\\d{1,2}\\S[aApP][mM])";
+	//private static final String Receivers   = "<([\\w\\d\\.]+@[\\w]+\\.[\\w]+)>";
+	private static final String Receivers   = "<([\\w\\d\\-\\.\\_]+@[\\w\\d\\-]+(\\.[\\w\\d]+)*(\\.[\\w]{2,}))>";
+    private static final Pattern p_emailStart = Pattern.compile(EmailStart);
+    private static final Pattern p_receivers = Pattern.compile(Receivers);
     
 	public HashMap<String, String> isEmailStart(String input) {
 		HashMap<String, String> resultset = new HashMap<String, String>();
-		Pattern p = Pattern.compile(EmailStart);
-		Matcher m = p.matcher(input); // get a matcher object
+		//Pattern p = Pattern.compile(EmailStart);
+		Matcher m = p_emailStart.matcher(input); // get a matcher object
 		if (m.lookingAt()) {
 			resultset.put("sender", m.group(1));
-			resultset.put("sendingtime", m.group(3));
+			resultset.put("sendingtime", m.group(5));
 		}
 		return resultset;
 	}
-	public String getReceivers(String input) {
-		Pattern p = Pattern.compile(Receivers);
-		Matcher m = p.matcher(input); // get a matcher object
-		if (m.lookingAt()) return m.group(1);
-		return null;
+	public ArrayList<String> getReceivers(String input) {
+		//System.out.println(input);
+		Matcher m = p_receivers.matcher(input); // get a matcher object
+		ArrayList<String> receivers = new ArrayList<String>();
+		
+		while (m.find()) receivers.add(m.group(1));
+		return receivers;
 	}
-	
-	/*
-	public ArrayList<String> getEmailContent(String input) {
-		Pattern p = Pattern.compile(Email);
-		Matcher m = p.matcher(input); // get a matcher object
-		int count = 0;
-		ArrayList<String> emails = new ArrayList<String>(); 
-		
-		
-		while (m.find()) {
-			count++;
-			System.out.println("Match number " + count);
-			System.out.println("start(): " + m.start());
-			System.out.println("end(): " + m.end());
-			//String text = input.substring(m.start(), m.end());
-			String text = m.group(0);
-			System.out.println(">>>" + text);
-			emails.add(text);
-		}
-		return emails;
-	}*/
 }
